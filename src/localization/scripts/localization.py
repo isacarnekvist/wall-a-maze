e653660669a3129c6fa592a8fab193a484c2d274
Isac Arnekvist <isac.arnekvist@gmail.com>
2016-10-13 20:08:04 +0200

    wip: first buggy version implemented

--- src/localization/scripts/localization.py
+++ src/localization/scripts/localization.py
@@ -4,11 +4,15 @@ from math import cos, sin
 from datetime import datetime
 
 import numpy as np
+import pandas as pd
 
 import rospy
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, Twist
 
 from map import Map
+
+N_LIDAR_MEASUREMENTS = 20
 
 
 class Localization():
@@ -18,11 +22,13 @@ class Localization():
 
         self.pose = PoseStamped()
         self.pose.header.frame_id = 'map'
+        self.map = Map(map_path='/home/ras26/catkin_ws/src/localization/scripts/map.txt')
 
         self.position_pub = rospy.Publisher('/position', PoseStamped, queue_size=10)
 
         self.odometry_timestamp = None
         self.kalman_timestamp = None
+        rospy.Subscriber('/scan', LaserScan, self.laser_callback)
         rospy.Subscriber('/odometry', Twist, self.odometry_callback)
         rospy.Subscriber('/motor_controller', Twist, self.control_callback)
         # add subscriber to laser scans in robot frame:
@@ -33,12 +39,22 @@ class Localization():
         self.linear_control = 0.0
         self.angular_control = 0.0
 
+        # keep track of change of state by odometry
+        self.x_delta = 0.0
+        self.y_delta = 0.0
+        self.theta_delta = 0.0
+
+        self.lidar_distances = np.zeros(360)
+
         # State:
         self.last_state_update = datetime.now()
-        self.x = 0.0
-        self.y = 0.0
+        self.x = 0.2
+        self.y = 0.2
         self.theta = 0.0
-        self.Gamma = np.eye(3) # Have velocities in state?
+        self.Sigma = 0.01 * np.eye(3)
+
+        self.jacobian = np.zeros((N_LIDAR_MEASUREMENTS, 3))
+        self.kalman_gain = np.zeros((3, 3))
 
         rate = rospy.Rate(125)
         while not rospy.is_shutdown():
@@ -54,29 +70,59 @@ class Localization():
         timedelta = datetime.now() - self.kalman_timestamp
         delta = timedelta.seconds + timedelta.microseconds / 1e6
 
-        theta_bar = self.theta + delta * self.angular_control
         mu_bar = np.array([
-            [self.x + delta * cos((self.theta + theta_bar) / 2) * self.linear_control],
-            [self.y + delta * sin((self.theta + theta_bar) / 2) * self.linear_control],
-            [theta_bar]
+            [self.x + self.x_delta],
+            [self.y + self.y_delta],
+            [self.theta + self.theta_delta]
         ])
+        self.x_delta = 0.0
+        self.y_delta = 0.0
+        self.theta_delta = 0.0
 
         # Update covariance matrix with noise
-        self.Gamma += np.diag([0.01, 0.01, 0.01]) # TODO update and tweak with how much we actually trust this update, maybe as function of theta?
+        # TODO update and tweak with how much we actually trust this update, maybe as function of theta?
+        self.Sigma = (
+            0.01 * np.eye(3) +
+            500 * np.eye(3) * delta * (np.abs(self.linear_control) + np.abs(self.angular_control))
+        )
 
         # Calculate kalman gain
         # 1: Get estimated laser readings given state
-        distances, dx, dy, dtheta = map.scan(*mu_bar.flatten())
+        distances, dx, dy, dtheta = self.map.scan(*mu_bar.flatten())
+        distances[distances == np.inf] = np.nan
         # 2: Get laser readings rotated in the same way
+        # done
         # 3: Compared signals and pick suitable subset
-        # 4: Build the jacobian and
+        diff = pd.Series(np.abs(self.lidar_distances - distances)).dropna()
+        diff.sort()
+        lidar_inds = diff.index[:N_LIDAR_MEASUREMENTS]
+        diff = diff[:N_LIDAR_MEASUREMENTS]
+
+        # 4: Build the jacobian
+        self.jacobian[:, 0] = dx[lidar_inds]
+        self.jacobian[:, 1] = dy[lidar_inds]
+        self.jacobian[:, 2] = dtheta[lidar_inds]
+
         # 5: Calculate kalman gain
+        self.kalman_gain = self.Sigma.dot(self.jacobian.T).dot(
+            np.linalg.inv(
+                self.jacobian.dot(self.Sigma).dot(self.jacobian.T) +
+                0.1 * np.eye(N_LIDAR_MEASUREMENTS)
+            ) # TODO tweak noise
+        )
 
         # Correction step
+        mu = mu_bar + self.kalman_gain.dot(diff.reshape((N_LIDAR_MEASUREMENTS, 1)))
+        self.Sigma = (np.eye(3) + self.kalman_gain.dot(self.jacobian)).dot(self.Sigma)
         # state = corrected state
         # Sigma = corrected Sigma
 
-        self.x, self.y, self.theta = mu_bar.flatten()
+        self.x, self.y, self.theta = mu.flatten()
+        if self.theta > 2 * np.pi:
+            self.theta -= 2 * np.pi
+        if self.theta < 0:
+            self.theta += 2 * np.pi
+        rospy.loginfo('Sigma total: {}'.format(np.abs(self.Sigma).sum()))
         rospy.loginfo('x: {} y: {} theta: {}'.format(self.x, self.y, self.theta))
 
         self.kalman_timestamp = datetime.now()
@@ -92,17 +138,24 @@ class Localization():
         self.linear_control = data.linear.x
         self.angular_control = data.angular.z
 
+    def laser_callback(self, data):
+        self.lidar_distances[:] = data.ranges
+        self.lidar_distances[self.lidar_distances == np.inf] = np.nan
+
     def odometry_callback(self, data):
         if self.odometry_timestamp is None:
             self.odometry_timestamp = datetime.now()
             return
 
         timedelta = datetime.now() - self.odometry_timestamp
+        self.odometry_timestamp = datetime.now()
         delta_seconds = timedelta.seconds + timedelta.microseconds / 1e6
+        self.x_delta += delta_seconds * cos(self.theta) * data.linear.x
+        self.y_delta += delta_seconds * sin(self.theta) * data.linear.x
+        self.theta_delta += delta_seconds * data.angular.z
 
         # Implement?
 
-        self.odometry_timestamp = datetime.now()
 
 if __name__ == '__main__':
     Localization()
