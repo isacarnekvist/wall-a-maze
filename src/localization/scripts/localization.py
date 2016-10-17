7cd3daacfdeef6c41140e1bc8a7da24b5dae6f9f
Isac Arnekvist <isac.arnekvist@gmail.com>
2016-10-17 17:50:36 +0200

    wip: rotated laser in localization

--- src/localization/scripts/localization.py
+++ src/localization/scripts/localization.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 
+import time
 from math import cos, sin
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
 
+import tf
 import rospy
-from sensor_msgs.msg import LaserScan
+from laser_geometry import LaserProjection
 from geometry_msgs.msg import PoseStamped, Twist
+from sensor_msgs.msg import LaserScan, PointCloud
 
 from map import Map
 
 N_LIDAR_MEASUREMENTS = 20
 
+
+def laserscan_to_coords(scan):
+    """Now the laser is transformed here due to lacking functionality in rospy"""
+    angles = np.arange(0, 360)
+    coords = np.array(map(
+        lambda x: [x[1] * np.cos(np.pi * x[0] / 180), x[1] * np.sin(np.pi * x[0] / 180)], zip(angles, scan.ranges)
+    ))
+
+    theta = 181.5 * np.pi / 180
+    rotation_matrix = np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta),  np.cos(theta)]
+    ])
+    return coords.dot(rotation_matrix)
+    
 
 class Localization():
 
@@ -22,17 +40,21 @@ class Localization():
 
         self.pose = PoseStamped()
         self.pose.header.frame_id = 'map'
-        self.map = Map(map_path='/home/ras26/catkin_ws/src/localization/scripts/map.txt')
+        self.map = Map(map_path='/home/ras26/catkin_ws/src/ras_maze/ras_maze_map/maps/lab_maze_2016.txt')
 
         self.position_pub = rospy.Publisher('/position', PoseStamped, queue_size=10)
 
         self.odometry_timestamp = None
         self.kalman_timestamp = None
+
+        self.lidar_coords = np.zeros((360, 2))
+        self.lidar_distances = np.zeros(360)
+        self.listener = tf.TransformListener()
+        self.laser_projector = LaserProjection()
+
         rospy.Subscriber('/scan', LaserScan, self.laser_callback)
         rospy.Subscriber('/odometry', Twist, self.odometry_callback)
         rospy.Subscriber('/motor_controller', Twist, self.control_callback)
-        # add subscriber to laser scans in robot frame:
-        # TODO
 
         # Start of changing to kalman filtering
         # Store control signals to estimate mu 
@@ -43,8 +65,6 @@ class Localization():
         self.x_delta = 0.0
         self.y_delta = 0.0
         self.theta_delta = 0.0
-
-        self.lidar_distances = np.zeros(360)
 
         # State:
         self.last_state_update = datetime.now()
@@ -63,6 +83,7 @@ class Localization():
             rate.sleep()
 
     def kalman_update(self):
+        return
         if self.kalman_timestamp is None:
             self.kalman_timestamp = datetime.now()
             return
@@ -141,6 +162,8 @@ class Localization():
     def laser_callback(self, data):
         self.lidar_distances[:] = data.ranges
         self.lidar_distances[self.lidar_distances == np.inf] = np.nan
+        self.lidar_coords = laserscan_to_coords(data)
+        print(self.lidar_coords[90, :])
 
     def odometry_callback(self, data):
         if self.odometry_timestamp is None:
